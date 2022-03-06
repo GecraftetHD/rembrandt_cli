47732f5bb12e52a6b5533423cb8599cda6f9bc98
GecraftetHD <77395698+GecraftetHD@users.noreply.github.com>
2022-03-07 00:27:51 +0100

    add: basic frontend implementation, added setup.py

--- rembrandt_cli/rembrandt.py
+++ rembrandt_cli/rembrandt.py
@@ -1,6 +1,7 @@
 import json
 import websocket
 import rembrandt_cli.exceptions
+import sys
 
 
 class Client:
@@ -124,3 +125,28 @@ class Client:
             raw_response = self.websocket.recv()
             response = json.loads(raw_response)
             return response
+
+def frontend():
+    while True:
+        command = input("$ ")
+        if command == "exit":
+            break
+        elif command == "help":
+            print("rembrandt_cli | a python based cryptic game client\n"
+                  " -help | list all commands\n"
+                  " -exit | stops the client\n")
+        else:
+            print(command)
+
+def main():
+    args = sys.argv[1:]
+    if len(args) > 0:
+        server = args[0]
+    else:
+        server = 'wss://ws.cryptic-game.net'
+
+    print("")
+    print("Welcome to rembrandt_cli")
+    print("")
+    print("GitHub: https://github.com/GecraftetHD/rembrandt_cli")
+    frontend()
